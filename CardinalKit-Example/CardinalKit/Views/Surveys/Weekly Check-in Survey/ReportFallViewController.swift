b1db93f3642b7ce39cbab2223d60440c0444301c
EstebanRamosFractal <46715376+EstebanRamosFractal@users.noreply.github.com>
2022-06-08 12:17:31 -0500

    Add Safety Check

--- CardinalKit-Example/CardinalKit/Views/Surveys/Weekly Check-in Survey/ReportFallViewController.swift
+++ CardinalKit-Example/CardinalKit/Views/Surveys/Weekly Check-in Survey/ReportFallViewController.swift
@@ -52,16 +52,24 @@ struct ReportFallViewController: UIViewControllerRepresentable {
             let injuryStep = ORKQuestionStep(identifier: "injuryStep", title: "Injury Associated with Fall", question: "Did this fall result in any type of injury?", answer: injuryChoiceAnswerFormat)
             
             steps += [injuryStep]
-//
-//            // brief description of fall
-//            let descriptionAnswerFormat = ORKAnswerFormat.textAnswerFormat(withMaximumLength: 100)
-//            let descriptionStep = ORKQuestionStep(identifier: "descriptionStep", title: "Brief Description of Fall", question: "Briefly give some details about your fall (optional).", answer: descriptionAnswerFormat)
-//
-//            steps += [descriptionStep]
             
+            let videoStep = VideoStep(identifier: "VideoInstructionsStep")
             
-            steps += [VideoStep(identifier: "VideoInstructionsStep")]
+            steps += [videoStep]
             
+            let safe1Question = ORKFormItem(identifier: "safe_1", text: "Have you cleared a 10-foot-long space with no throw rugs or obstructions?", answerFormat: ORKBooleanAnswerFormat(yesString: "yes", noString: "no"))
+            
+            let safe2Question = ORKFormItem(identifier: "safe_2", text: "Have you set up a chair on one side (ideally with arms)?", answerFormat: ORKBooleanAnswerFormat(yesString: "yes", noString: "no"))
+
+            let safe3Question = ORKFormItem(identifier: "safe_3", text: "Are you wearing regular footwear?", answerFormat: ORKBooleanAnswerFormat(yesString: "yes", noString: "no"))
+            let safe4Question = ORKFormItem(identifier: "safe_4", text: "Do you have someone present who can assist you if needed?", answerFormat: ORKBooleanAnswerFormat(yesString: "yes", noString: "no"))
+
+            let safe5Question = ORKFormItem(identifier: "safe_5", text: "Have you put on the belt with pouch to hold your phone?", answerFormat: ORKBooleanAnswerFormat(yesString: "yes", noString: "no"))
+
+            let safetyForm = ORKFormStep(identifier: "safetyCheck", title: "Safety Check", text: "Ensure that each of these safety conditions is met prior to your mobility assessment")
+            safetyForm.formItems = [safe1Question, safe2Question, safe3Question, safe4Question, safe5Question]
+            
+            steps += [safetyForm]
             
             return ORKOrderedTask(identifier: "SurveyTask", steps: steps)
         }()
