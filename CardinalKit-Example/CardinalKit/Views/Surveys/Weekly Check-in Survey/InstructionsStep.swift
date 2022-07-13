1f70030b4b21d61c2dca2fef41282a0bafc4ce1c
EstebanRamosFractal <46715376+EstebanRamosFractal@users.noreply.github.com>
2022-07-13 17:44:35 -0500

    Update Weekly survey ui view

--- CardinalKit-Example/CardinalKit/Views/Surveys/Weekly Check-in Survey/InstructionsStep.swift
+++ CardinalKit-Example/CardinalKit/Views/Surveys/Weekly Check-in Survey/InstructionsStep.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import ResearchKit
+import UIKit
 
 public class InstructionsStep: ORKInstructionStep{
     public override init(identifier: String) {
@@ -26,8 +27,39 @@ public class InstructionsStep: ORKInstructionStep{
 
 public class InstructionsStepViewController: ORKInstructionStepViewController{
     override public func viewDidLoad() {
-        self.continueButtonTitle = "Ready"
-        super.viewDidLoad()
+        let buttonApple = CustomButton(title: "Ready", backGroundColor: UIColor(red: 242, green: 177, blue: 55), textColor: .white, borderColor: nil, action: #selector(action), location: CGRect(x: 200, y: 200, width: 350, height: 75))
+        self.view.addSubview(buttonApple)
+        self.view.backgroundColor = UIColor(red: 110, green: 237, blue: 251)
+    }
+    
+    
+    public func CustomButton(
+        title:String,
+        backGroundColor:UIColor,
+        textColor:UIColor,
+        borderColor:UIColor?,
+        action: Selector,
+        location: CGRect
+    )->UIButton{
+        let button = UIButton(frame: location)
+        button.center = view.center
+        button.center.y = view.center.y - 200
+        button.setTitle(title, for: .normal)
+        
+        button.setTitleColor(textColor,for: .normal)
+        button.addTarget(self,action: action,for: .touchUpInside)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = backGroundColor
+        if let borderColor=borderColor{
+            button.layer.borderWidth = 2
+            button.layer.borderColor =  borderColor.cgColor
+        }
+        return button
+    }
+    
+    @objc
+    func action(){
+        super.goForward()
     }
 }
 
--- CardinalKit-Example/CardinalKit/Views/Surveys/Weekly Check-in Survey/WalkStep.swift
+++ CardinalKit-Example/CardinalKit/Views/Surveys/Weekly Check-in Survey/WalkStep.swift
@@ -37,7 +37,38 @@ public class WalkStep: ORKWalkingTaskStep{
 
 public class WalkStepViewController: ORKWalkingTaskStepViewController{
     override public func viewDidLoad() {
-        self.skipButtonTitle = "Stop"
-        super.viewDidLoad()
+        let buttonDone = CustomButton(title: "Done", backGroundColor: UIColor(red: 242, green: 177, blue: 55), textColor: .white, borderColor: nil, action: #selector(action), location: CGRect(x: 200, y: 200, width: 350, height: 75))
+        self.view.addSubview(buttonDone)
+        self.view.backgroundColor = UIColor(red: 110, green: 237, blue: 251)
+    }
+    
+    
+    public func CustomButton(
+        title:String,
+        backGroundColor:UIColor,
+        textColor:UIColor,
+        borderColor:UIColor?,
+        action: Selector,
+        location: CGRect
+    )->UIButton{
+        let button = UIButton(frame: location)
+        button.center = view.center
+        button.center.y = view.center.y - 200
+        button.setTitle(title, for: .normal)
+        
+        button.setTitleColor(textColor,for: .normal)
+        button.addTarget(self,action: action,for: .touchUpInside)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = backGroundColor
+        if let borderColor=borderColor{
+            button.layer.borderWidth = 2
+            button.layer.borderColor =  borderColor.cgColor
+        }
+        return button
+    }
+    
+    @objc
+    func action(){
+        super.goForward()
     }
 }
